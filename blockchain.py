7eb4004795c3d3fd0d9070dd45a4558f8802fe58
Tirdea Alexandru <sanydge@gmail.com>
2019-10-09 11:44:21 +0300

    more about functions(test)

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,34 @@
-blockchain = [1, 2, 6]
+#Initializing our blockchain list
+blockchain = []
 
-def add_value():
-    blockchain.append(9)
-    print (blockchain)
 
-add_value()
+def get_last_blockchain_value():
+    """Returns the last value of the curent blockchain. """
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction=[1]):
+    """Append a new value as well as the last blockchain value to the blockchain.
+    :argument
+        :transaction_amount: the amount that should be added.
+        :last_transaction: the last blockchain transaction(default[1].)
+    """
+    blockchain.append([last_transaction, transaction_amount])
+
+
+def get_user_input():
+    """Returns the input of the user (a new transaction amount) as a float. """
+    user_input = float(input('Your transaction amout please:'))
+    return user_input
+
+
+tx_amount = get_user_input()
+add_value(tx_amount)
+
+tx_amount = get_user_input()
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=0.9)
+
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
+
+print(blockchain)
