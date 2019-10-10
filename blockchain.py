fc00c2e53923c83ccd1da67bcd97e5fb7ef0b8ca
Tirdea Alexandru <sanydge@gmail.com>
2019-10-10 17:40:05 +0300

    adding conditionals (if-else-elif)

--- blockchain.py
+++ blockchain.py
@@ -4,34 +4,56 @@ blockchain = []
 
 def get_last_blockchain_value():
     """Returns the last value of the curent blockchain. """
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
     """Append a new value as well as the last blockchain value to the blockchain.
     :argument
         :transaction_amount: the amount that should be added.
         :last_transaction: the last blockchain transaction(default[1].)
     """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     """Returns the input of the user (a new transaction amount) as a float. """
-    user_input = float(input('Your transaction amout please:'))
+    user_input = float(input('Your transaction amount please:'))
     return user_input
 
 
-tx_amount = get_user_input()
-add_value(tx_amount)
+def get_user_choice():
+    user_input = input('Your choice:')
+    return user_input
 
-while True:
-    tx_amount = get_user_input()
-    add_value(tx_amount, get_last_blockchain_value())
 
+def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+
+
+while True:
+    print('Please choose')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('q: Quit')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_transaction(tx_amount, get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'q':
+        break
+    else:
+        print('Input was invalid, please pick a value from the list!')
+    print('Choice registered!')
+
 
 print('Done!')
 
